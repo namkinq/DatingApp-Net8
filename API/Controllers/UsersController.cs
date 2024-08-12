e1f6f97112d66fa5f0fae3fb85fc5ff0f65d73a7
HongNguyen <nguyenhongpf@gmail.com>
2024-08-13 01:23:22 +0700

    Extend API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,26 +1,32 @@
 using System;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet] // /api/users
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-        var users = await context.Users.ToListAsync();
-        return users;
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    {
+        var users = await userRepository.GetMembersAsync();
+
+        return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id:int}")] // /api/users/3
-    public async Task<ActionResult<AppUser>> GetUsers(int id){
-        var user = await context.Users.FindAsync(id);
+    [HttpGet("{username}")] // /api/users/name
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
+    {
+        var user = await userRepository.GetMemberAsync(username);
         if(user ==null) return NotFound();
+
         return user;
     }
 }

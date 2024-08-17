9c15c21fe746ded8c657170aefc11e6b4c5b62aa
HongNguyen <nguyenhongpf@gmail.com>
2024-08-18 01:29:20 +0700

    Build user interface

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,8 +7,8 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
-    public byte[] PasswordHash { get; set; }
-    public byte[] PasswordSalt { get; set; }
+    public byte[] PasswordHash { get; set; } = [];
+    public byte[] PasswordSalt { get; set; } = [];
     public DateOnly DateOfBirth { get; set; }
     public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;

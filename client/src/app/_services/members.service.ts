4f31efa8a6c57baa702fec4ca66ab3cc1b321718
HongNguyen <nguyenhongpf@gmail.com>
2024-08-26 01:24:59 +0700

    add photo upload func

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -4,6 +4,7 @@ import { environment } from '../../environments/environment';
 import { AccountService } from './account.service';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/photo';
 
 @Injectable({
   providedIn: 'root'
@@ -32,6 +33,31 @@ export class MembersService {
         this.members.update(members => members.map(m => m.userName === member.userName
           ? member : m
         ))
+      })
+    )
+  }
+  setMainPhoto(photo: Photo){
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' +photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m =>{
+          if(m.photos.includes(photo)){
+            m.photoUrl = photo.url
+          }
+          return m;
+        }))
+      })
+    )
+  }
+
+  deletePhoto(photo: Photo){
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => {
+          if(m.photos.includes(photo)){
+            m.photos = m.photos.filter(x => x.id !== photo.id)
+          }
+          return m;
+        }))
       })
     )
   }

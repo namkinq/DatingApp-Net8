c60c7772ae3fbd8d03836d646af70c3796ce4fa6
HongNguyen <nguyenhongpf@gmail.com>
2024-08-27 02:24:41 +0700

    Reactive forms

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,7 +9,18 @@ public class RegisterDto
     public string Username { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(8, MinimumLength = 4)]
+    public string? KnownAs { get; set; }
+    [Required]
+    public string? Gender { get; set; }
+    [Required]
+    public string? DateOfBirth { get; set; }
+    [Required]
+    public string? City { get; set; }
+    [Required]
+    public string? Country { get; set; }
+
+    [Required]
+    [StringLength(40, MinimumLength = 6)]
     public string Password { get; set; } = string.Empty;
 
 
